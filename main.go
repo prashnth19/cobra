6c872caaffda487bf24726515a6bbb014892362a
prashnth19 <142342803+prashnth19@users.noreply.github.com>
2025-09-16 17:22:33 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/spf13/cobra"
+	// External imports
+	"github.com/spf13/cobra"         // single-line import
+	"github.com/google/uuid"         // another external dep
+	"golang.org/x/text/cases"        // golang.org package
+	"golang.org/x/text/language"     // paired dependency
+
+	// Aliased import
+	uuidv4 "github.com/google/uuid"
+
+	// Blank import (side-effect only)
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	// Use cobra
 	var rootCmd = &cobra.Command{
 		Use:   "hello",
 		Short: "Prints hello",
@@ -14,6 +27,21 @@ func main() {
 			fmt.Println("Hello, ORT Recovery!")
 		},
 	}
+
+	// Use uuid
+	id := uuid.New()
+	fmt.Println("Generated UUID:", id)
+
+	// Use aliased uuid
+	id2 := uuidv4.New()
+	log.Println("Generated UUID (aliased):", id2)
+
+	// Use text casing
+	caser := cases.Title(language.English)
+	fmt.Println(caser.String("ort recovery test"))
+
+	// Just to keep os in use
+	fmt.Println("Args:", os.Args)
 
 	rootCmd.Execute()
 }
